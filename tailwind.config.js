a9570c61f26bcbc5e4ba58a049b8d38b98c77bab
alignitems <align.items@gmail.com>
2022-07-10 15:53:54 +0900

    ✨ Use Roboto mono for UI, Roboto for times

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,8 @@
-tailwind.config = {}
+tailwind.config = {
+    theme: {
+        fontFamily: {
+            'sans': ['Roboto', 'sans-serif'],
+            'mono': ['"Roboto Mono"', 'monospace']
+        }
+    }
+}
